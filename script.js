a541068f957e30c27080c3678f24df3d5e5c96bf
fahmamisria-ops <fahmamisria@gmail.com>
2025-11-18 15:38:45 +0530

    Add calculator files

---
+++ script.js
@@ -0,0 +1,18 @@
+function clearScreen() {
+    document.getElementById("result").value = "";
+}
+
+function insert(value) {
+    document.getElementById("result").value += value;
+}
+
+function calculate() {
+    var p = document.getElementById("result").value;
+    var q = eval(p);
+    document.getElementById("result").value = q;
+}
+
+function deleteLast() {
+    var value = document.getElementById("result").value;
+    document.getElementById("result").value = value.substr(0, value.length - 1);
+}
